c0b923999dbcc015912953ea7e8c54b410a53a0a
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2022-12-17 22:02:11 +0300

    added env and config.env file

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express();
 
-app.listen(8080, () => {
-    console.log('server is running');
+dotenv.config({ path: './src/config/config.env' });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`);
 })
