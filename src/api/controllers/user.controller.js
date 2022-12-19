0ba8f0fcffd167ebc93a55093cc4cd781c97a0f9
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2022-12-19 22:22:34 +0300

    user service was used in user controller

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,12 +1,13 @@
-const { userMock } = require('../../mock');
+const { userService } = require('../services');
 
 // @desc   Get All Users
 // @route  GET /users
 // @access Public
 exports.getAllUsers = (req, res, next) => {
+    const users = userService.getAllUsers();
     const response = {
         success: true,
-        data: userMock
+        data: users
     }
     res.status(200).send(response);
 };
