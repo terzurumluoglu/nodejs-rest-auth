b56195d55de2db0a4bb4d4a0823fc615c5d39b90
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2022-12-31 19:54:18 +0300

    token save cookie

--- src/api/services/auth.service.js
+++ src/api/services/auth.service.js
@@ -1,6 +1,11 @@
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { asyncHandler } = require('../middleware/asyncHandler');
+const { environments } = require('../../constants');
+
+const service = require('../services');
+
+const dayAsSecond = 24 * 60 * 60 * 1000;
 
 exports.hashString = asyncHandler(async (str) => {
     const salt = await bcryptjs.genSalt(10);
@@ -11,8 +16,35 @@ exports.matchPassword = (hashedPassword, enteredPassword) => {
     return bcryptjs.compare(enteredPassword, hashedPassword);
 };
 
-exports.generateJWT = function(id) {
+exports.generateJWT = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
 };
+
+exports.sendTokenResponse = (user, statusCode, res) => {
+
+    const userInfo = service.userService.getUserInfo(user);
+
+    const token = this.generateJWT(userInfo.id);
+
+    const now = new Date();
+    const expires = new Date(now + process.env.JWT_COOKIE_EXPIRE * dayAsSecond);
+
+    const options = {
+        expires,
+        httpOnly: true,
+    };
+
+    if (process.env.ENVIRONMENT === environments.production) {
+        options.secure = true;
+    }
+
+    res.status(statusCode).cookie('token', token, options).send({
+        success: true,
+        data: {
+            ...userInfo,
+            token
+        }
+    })
+}
