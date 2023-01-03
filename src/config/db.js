9e591794b6b149416b4c46cb4a1c740d91ef719f
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-03 18:29:21 +0300

    Database name seperated from connectionstring

--- src/config/db.js
+++ src/config/db.js
@@ -4,16 +4,17 @@ mongoose.set("strictQuery", false);
 
 const connectDatabase = async () => {
 
+    const name = process.env.DB_NAME;
     const username = process.env.DB_USERNAME;
     const password = process.env.DB_PASSWORD;
 
     const connectionString = process.env.CONNECTION_STRING
         .replace('{{USERNAME}}', username)
-        .replace('{{PASSWORD}}', password);
+        .replace('{{PASSWORD}}', password)
+        .replace('{{NAME}}', name);
 
     const con = await mongoose.connect(connectionString);
-    const { name } = con.connection;
-    console.log(`db connection was creaated successfully! Database: ${name}`);
+    console.log(`db connection was creaated successfully! Database: ${con.connection.name}`);
 }
 
 module.exports = { connectDatabase };
