64488203b27c69170d17338f346773d4e56f4397
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-04 09:02:29 +0300

    changing mongoose to mongodb

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,37 +1,40 @@
-const User = require('../models/User');
 const { asyncHandler } = require('../middleware/asyncHandler');
-const service = require('../services');
+const { authService } = require('../services');
 const ErrorResponse = require('../utils/ErrorResponse');
+const { getDatabase } = require('../../config/db');
+
+
+const getUserCollection = () => getDatabase().collection('users');
 
 // @desc   Register
 // @route  POST /auth/register
 // @access Public
-exports.register = asyncHandler(async (req, res, next) => {
+const register = asyncHandler(async (req, res, next) => {
 
     const { name, email, password } = req.body;
 
-    const user = new User({
+    if ([name, email, password].includes(undefined)) {
+        return next(new ErrorResponse('Name, Email, Password must enter!', 400));
+    }
+
+    const userToBeInserted = {
         name,
         email,
-        password
-    });
+        password: await authService.hashString(password),
+    }
 
-    const response = await user.save();
+    const collection = getUserCollection();
+    const { insertedId } = await collection.insertOne(userToBeInserted);
 
-    const access_token = service.authService.generateJWT();
+    const user = await collection.findOne({ _id: insertedId });
 
-    const userInfo = service.userService.getUserInfo(response);
-
-    res.status(200).json({
-        success: true,
-        data: { ...userInfo, access_token },
-    });
+    authService.sendTokenResponse(user, 200, res);
 });
 
 // @desc   Login
 // @route  POST /auth/login
 // @access Public
-exports.login = asyncHandler(async (req, res, next) => {
+const login = asyncHandler(async (req, res, next) => {
 
     const { email, password } = req.body;
 
@@ -39,17 +42,19 @@ exports.login = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse('Email and Password must enter', 400));
     }
 
-    const user = await User.findOne({ email }).select('+password');
+    const user = await getUserCollection().findOne({ email });
 
     if (!user) {
         return next(new ErrorResponse('Email or Password is invalid!', 401));
     }
 
-    const isMatch = await user.matchPassword(password);
+    const isMatch = await authService.matchPassword({ enteredPassword: password, hashedPassword: user.password });
 
     if (!isMatch) {
         return next(new ErrorResponse('Email or Password is invalid!', 401));
     }
 
-    service.authService.sendTokenResponse(user, 200, res);
+    authService.sendTokenResponse(user, 200, res);
 });
+
+module.exports = { register, login };
