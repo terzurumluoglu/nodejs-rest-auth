8c3dbf193fc51d10a2b09bf96d0a1f300e5ede35
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-04 09:01:22 +0300

    changed exoprt way and deleted unused code

--- src/api/services/auth.service.js
+++ src/api/services/auth.service.js
@@ -1,32 +1,31 @@
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { asyncHandler } = require('../middleware/asyncHandler');
 const { environments } = require('../../constants');
 
-const service = require('../services');
+const { getUserInfo } = require('../services/user.service');
 
 const dayAsSecond = 24 * 60 * 60 * 1000;
 
-exports.hashString = asyncHandler(async (str) => {
+const hashString = async (str) => {
     const salt = await bcryptjs.genSalt(10);
     return bcryptjs.hash(str, salt);
-});
+};
 
-exports.matchPassword = (hashedPassword, enteredPassword) => {
+const matchPassword = ({ enteredPassword, hashedPassword }) => {
     return bcryptjs.compare(enteredPassword, hashedPassword);
 };
 
-exports.generateJWT = (id) => {
+const generateJWT = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
 };
 
-exports.sendTokenResponse = (user, statusCode, res) => {
+const sendTokenResponse = (user, statusCode, res) => {
 
-    const userInfo = service.userService.getUserInfo(user);
+    const userInfo = getUserInfo(user);
 
-    const token = this.generateJWT(userInfo.id);
+    const token = generateJWT(userInfo.id);
 
     const now = (new Date()).getTime();
     const expires = new Date(now + process.env.JWT_COOKIE_EXPIRE * dayAsSecond);
@@ -48,3 +47,5 @@ exports.sendTokenResponse = (user, statusCode, res) => {
         }
     })
 }
+
+module.exports = { hashString, matchPassword, generateJWT, sendTokenResponse };
