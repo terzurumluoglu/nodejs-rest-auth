64b5f6c21476b275d3e60d82d08e060de84915da
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-04 23:59:22 +0300

    Mail class created

---
+++ src/api/utils/Mail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+class Mail {
+    client;
+    constructor() {
+        this.client = nodemailer.createTransport({
+            service: process.env.MAIL_SERVICE,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        });
+    }
+
+    sendEmail = async ({ to, subject, text }) => {
+        const mailOptions = {
+            from: process.env.MAIL_USER,
+            to,
+            subject,
+            text
+        };
+        await this.client.sendMail(mailOptions);
+    };
+
+}
+
+module.exports = Mail;
