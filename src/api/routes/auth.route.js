6fca625ced4d241791195871500cd6494292a5f4
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-05 00:06:34 +0300

    added forgotpassword and resetpassword routes

--- src/api/routes/auth.route.js
+++ src/api/routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { login, register } = require('../controllers/auth.controller');
+const { register, login, forgotPassword, resetPassword } = require('../controllers/auth.controller');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/forgotpassword').post(forgotPassword);
+router.route('/resetpassword/:resetPasswordKey').post(resetPassword);
 
 module.exports = router;
