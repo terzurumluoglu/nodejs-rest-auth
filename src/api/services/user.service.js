5c0260532989a1f633057c40332c59b264a3060d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-04 09:00:47 +0300

    changed export way

--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -1,4 +1,6 @@
-exports.getUserInfo = (userResponse) => {
+const getUserInfo = (userResponse) => {
     const { _id, name, email, createdAt } = userResponse;
     return { id: _id, name, email, createdAt };
 }
+
+module.exports = { getUserInfo };
