ba782a2263b1a15eda9f268d51bcdf7c3e4ddf2d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-01-06 11:55:53 +0300

    user model changed for only about user things

--- src/api/models/user.js
+++ src/api/models/user.js
@@ -1,23 +1,45 @@
-const { getUserCollection } = require('../../config/db');
-const { authService } = require('../services');
-
 class User {
+    _id;
     name;
     email;
     password;
     createdAt;
+
+    isActive;
+    isDeleted;
+    resetPasswordKey;
+    resetPasswordKeyExpire;
     constructor() {
         this.createdAt = Date.now();
+        this.isActive = true;
+        this.isDeleted = false;
+    }
+
+    get info() {
+        return {
+            _id: this._id,
+            name: this.name,
+            email: this.email,
+            createdAt: this.createdAt,
+            isActive: this.isActive,
+            isDeleted: this.isDeleted,
+        }
     }
 
     set setName(val) {
-        if (!val || val.length < 3) {
+        if (!val) {
+            throw new Error('Name must enter!');
+        }
+        if (val.length < 3) {
             throw new Error('Name must be greather than 2 characters!');
         }
         this.name = val;
     }
 
     set setEmail(val) {
+        if (!val) {
+            throw new Error('Email must be enter');
+        }
         const isValidate = String(val)
             .toLowerCase()
             .match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
@@ -40,24 +62,16 @@ class User {
         this.password = val;
     }
 
-    saveUser = async () => {
-        const userToBeInserted = {
-            name: this.name,
-            email: this.email,
-            password: await authService.hashString(this.password),
-            createdAt: this.createdAt,
-        };
-        const { insertedId } = await getUserCollection().insertOne(userToBeInserted);
-
-        return collection.findOne({ _id: insertedId });
-    }
-
-    getUserById = (id) => {
-        return getUserCollection().findOne({ _id: id });
-    }
-
-    getUserByEmail = (email) => {
-        return getUserCollection().findOne({ email });
+    set setUser(val) {
+        this._id = val._id;
+        this.name = val.name;
+        this.email = val.email;
+        this.password = val.password;
+        this.createdAt = val.createdAt;
+        this.isActive = val.isActive;
+        this.isDeleted = val.isDeleted;
+        this.resetPasswordKey = val.resetPasswordKey;
+        this.resetPasswordKeyExpire = val.resetPasswordKeyExpire;
     }
 }
 
