7d051904a91a5aa7a430a24ee28ebf038dd5acfe
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-05-08 21:53:50 +0300

    token end point

--- src/api/routes/auth.route.js
+++ src/api/routes/auth.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, forgotPassword, resetPassword } = require('../controllers/auth.controller');
+const { register, login, forgotPassword, resetPassword, token } = require('../controllers/auth.controller');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/forgotpassword').post(forgotPassword);
 router.route('/resetpassword/:resetPasswordKey').post(resetPassword);
+router.route('/token').post(token);
 
 module.exports = router;
